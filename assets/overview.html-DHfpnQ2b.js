7463c680c30959e9afa723c5337d88c327f66081
cahyosubroto <91090730+cahyosubroto@users.noreply.github.com>
2024-12-12 06:30:50 +0000

    Deploying to gh-pages from @ cahyosubroto/cahyosubroto.github.io@1017e95aa54fd65a0d38d9404b786758d750247a 🚀

---
+++ assets/overview.html-DHfpnQ2b.js
@@ -0,0 +1,41 @@
+import{_ as a}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as i,b as t,a as s,d as n,o as r,r as l}from"./app-ZeD48GL0.js";const d={},c={class:"cards"},p={class:"cards"};function g(u,o){const e=l("VPCard");return r(),i("div",null,[o[0]||(o[0]=t("p",null,"Zillabase is a modern alternative to Firebase, designed for real-time data processing and asynchronous workloads. It integrates a suite of streaming-native open-source tools to automatically build an event-drivent backend. This includes real-time data models, user-defined functions, robust security, and versatile APIs (supporting both RESTful and asynchronous interactions).",-1)),o[1]||(o[1]=t("h2",{id:"getting-started",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#getting-started"},[t("span",null,"Getting Started")])],-1)),t("div",c,[s(e,{title:"Overview",logo:"/assets/icons/overview.svg",link:"/getting-started/overview"}),s(e,{title:"Quickstart",logo:"/assets/icons/quickstart.svg",link:"/getting-started/quickstart"})]),o[2]||(o[2]=t("h2",{id:"products",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#products"},[t("span",null,"Products")])],-1)),t("div",p,[s(e,{title:"Zilla",logo:"/assets/icons/zilla_solid.svg",link:"/products/zilla/generated-asyncapi-specs"}),s(e,{title:"Table",logo:"/assets/icons/table_solid.svg",link:"/products/table/defining-table"}),s(e,{title:"View",logo:"/assets/icons/views_solid.svg",link:"/products/view/defining-view"}),s(e,{title:"Stream",logo:"/assets/icons/streams_solid.svg",link:"/products/stream/defining-stream"}),s(e,{title:"Function",logo:"/assets/icons/function_solid.svg",link:"/products/function/user-defined-function"}),s(e,{title:"Authentication",logo:"/assets/icons/auth_solid.svg",link:"/products/authentication/concepts"}),n(` <VPCard
+    title="Storage"
+    logo="/assets/icons/storage_solid.svg"
+    link="/products/storage/concepts"
+  /> `)]),n(` ## Sample Application
+
+<div class="cards">
+  <VPCard
+    title="StreamPay"
+    logo="/assets/icons/streampay.svg"
+    link="/sample-app/streampay"
+  />
+
+  <VPCard
+    title="PetStore"
+    logo="/assets/icons/petstore.svg"
+    link="/sample-app/petstore"
+  />
+</div>
+
+## Support
+
+<div class="cards">
+  <VPCard
+    title="Troubleshooting"
+    logo="/assets/icons/troubleshoot.svg"
+    link="/support/troubleshooting"
+  />
+
+  <VPCard
+    title="FAQs"
+    logo="/assets/icons/faqs.svg"
+    link="/support/faqs"
+  />
+
+  <VPCard
+    title="Community Support"
+    logo="/assets/icons/support.svg"
+    link="/support/community-support"
+  />
+</div> `)])}const h=a(d,[["render",g],["__file","overview.html.vue"]]),f=JSON.parse('{"path":"/getting-started/overview.html","title":"What is Zillabase?","lang":"en-US","frontmatter":{"title":"What is Zillabase?","description":"Zillabase is a modern alternative to Firebase, designed for real-time data processing and asynchronous workloads. It integrates a suite of streaming-native open-source tools to ...","head":[["meta",{"property":"og:url","content":"https://docs.aklivity.io/getting-started/overview.html"}],["meta",{"property":"og:site_name","content":"Documentation"}],["meta",{"property":"og:title","content":"What is Zillabase?"}],["meta",{"property":"og:description","content":"Zillabase is a modern alternative to Firebase, designed for real-time data processing and asynchronous workloads. It integrates a suite of streaming-native open-source tools to ..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2024-12-12T06:21:56.000Z"}],["meta",{"property":"article:modified_time","content":"2024-12-12T06:21:56.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"What is Zillabase?\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-12-12T06:21:56.000Z\\",\\"author\\":[]}"]]},"headers":[{"level":2,"title":"Getting Started","slug":"getting-started","link":"#getting-started","children":[]},{"level":2,"title":"Products","slug":"products","link":"#products","children":[]}],"git":{"createdTime":1732866505000,"updatedTime":1733984516000,"contributors":[{"name":"cahyosubroto","email":"cahyosubroto30@gmail.com","commits":7}]},"readingTime":{"minutes":0.68,"words":205},"filePathRelative":"getting-started/overview.md","localizedDate":"November 29, 2024","autoDesc":true,"excerpt":"<p>Zillabase is a modern alternative to Firebase, designed for real-time data processing and asynchronous workloads. It integrates a suite of streaming-native open-source tools to automatically build an event-drivent backend. This includes real-time data models, user-defined functions, robust security, and versatile APIs (supporting both RESTful and asynchronous interactions).</p>"}');export{h as comp,f as data};
