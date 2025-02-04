f89b5d19a299fdde8ce7d980d1f083cfbde20a69
cahyosubroto <cahyosubroto30@gmail.com>
2025-02-04 17:10:39 +0700

    manual deploy zilla docs

--- zilla-docs/next/assets/before-after-zilla-DyPwRVMu.js
+++
@@ -1,1 +0,0 @@
-const o="/zilla-docs/next/zilla-overview.png",s="/zilla-docs/next/before-after-zilla.svg";export{o as _,s as a};
---
+++ zilla-docs/next/assets/zilla-motion-rEJ2PrCX.js
@@ -0,0 +1,1 @@
+const s="/zilla-docs/next/assets/zilla-motion.gif";export{s as _};
